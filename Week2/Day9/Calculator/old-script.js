aa11f6aa0a8437b243990649b639fd7c73fae60f
alirabah93 <abu.rabah.93@gmail.com>
2022-09-05 11:16:02 +0300

    My SQL_user assignment

--- Week2/Day9/Calculator/old-script.js
+++ Week2/Day9/Calculator/old-script.js
@@ -1,32 +1,23 @@
-// // Note for the reader: In this assignment, I used the original scripts from CodingDojo Calculator and 
-// fixed the following bug at it: (accidently pressing "function setOP()" more than once will cause an error 
-// in lines 12-16 in the old-script. calling the function twice will srt num2 value to nothing)
 const display = document.getElementById("display");
 let num1 = "";
 let num2 = "";
-let num3 = "";
 let op = "";
+
 
 function press(num) {
   num1 += num;
-  num3 += num;
-  display.innerHTML = num3;
-  if (num1 !== num3){
-    display.innerHTML = num2 + op + num1;
-  }
+  display.innerHTML = num1;
 }
 
 function setOP(key) {
-    op = key;
-    num2 = num3;
-    num1 = "";
-    display.innerHTML += op;
+  op = key;
+  num2 = num1;
+  num1 = "";
 }
 
 function clr() {
   num1 = "";
   num2 = "";
-  num3 = "";
   op = "";
   display.innerHTML = "0";
 }
@@ -50,7 +41,6 @@ function calculate() {
       break;
   }
   num1 = res;
-  num3 = res;
   op = "";
   display.innerHTML = res;
 }
