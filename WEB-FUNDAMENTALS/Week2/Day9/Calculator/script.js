f32e2304e9a29d67b42b941eb4a4aeddd8280e51
alirabah93 <abu.rabah.93@gmail.com>
2022-09-06 12:57:32 +0300

    second bug-fix

--- WEB-FUNDAMENTALS/Week2/Day9/Calculator/script.js
+++ WEB-FUNDAMENTALS/Week2/Day9/Calculator/script.js
@@ -1,29 +1,46 @@
-// Note for the reader: In this assignment, I used the original scripts from CodingDojo's Calculator and 
-// fixed the following bug:
-// (accidentally pressing "function setOP()" in lines 12-16 at the original script more than once or try to change your setOP to 
-// different value will cause an error. display.innerHTML = NaN)
-// additional features: display.innerHTML will show your whole input 
-// reader can view changes using the "Original script from coding dojo" commit and "script_bug_fix" commit
+// // (first bug-fix) Note for the reader: In this assignment, I used the original scripts from CodingDojo Calculator and 
+// fixed the following bug at it: (accidently pressing "function setOP()" more than once will cause an error 
+// in lines 12-16 in the old-script. calling the function twice will srt num2 value to nothing)
+// (second bug-fix) in the previous script, when you try to make more Mathematical equation after 
+// calculating it will show you "NaN" at the display 
 const display = document.getElementById("display");
 let num1 = "";
 let num2 = "";
 let num3 = "";
+let num4 = "";
+let num5 = "";
 let op = "";
 
 function press(num) {
   num1 += num;
   num3 += num;
+  num4 += num;
   display.innerHTML = num1;
-  if (num1 !== num3){
-    display.innerHTML = num2 + op + num1;
-  }
+  display.innerHTML = num2 + op + num1;
+  console.log("number1:" + num1);
+  console.log("number2:" + num2);
+  console.log("number3:" + num3);
+  console.log("number4:" + num4);
+  console.log("number5:" + num5);
 }
 
 function setOP(key) {
+  if (num1 == num2){
+    num2 = num1;
+  } else if (num3 !== num4){
+    calculate();
+  } else num2 = num3;
     op = key;
-    num2 = num3;
     num1 = "";
+    num4 = "";
     display.innerHTML += op;
+    console.log("number1:" + num1);
+    console.log("number2:" + num2);
+    console.log("number3:" + num3);
+    console.log("number4:" + num4);
+    console.log("number5:" + num5);
+    console.log("action:" + op);
+    
 }
 
 function clr() {
@@ -53,7 +70,9 @@ function calculate() {
       break;
   }
   num1 = res;
-  num3 = res;
+  num5 = res;
+  num2 = num5;
+  num4 = "";
   op = "";
   display.innerHTML = res;
 }
