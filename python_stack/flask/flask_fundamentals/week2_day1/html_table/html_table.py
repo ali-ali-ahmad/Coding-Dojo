52fd3edacd6bbdfd74099993ba5482a917e49c39
alirabah93 <abu.rabah.93@gmail.com>
2022-09-18 15:18:12 +0300

    edit the # section to generate numbers

--- python_stack/flask/flask_fundamentals/week2_day1/html_table/html_table.py
+++ python_stack/flask/flask_fundamentals/week2_day1/html_table/html_table.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def users_list():
     users_info = [
-        {'first_name' : 'Michael', 'last_name' : 'Choi'},
-        {'first_name' : 'John', 'last_name' : 'Supsupin'},
-        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'id':1, 'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'id':2, 'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'id':3, 'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'id':4, 'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
     return render_template("index.html", users = users_info)
 
