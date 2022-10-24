051348a8090e1ea88dc28c7b724669dd88e2530a
alirabah93 <abu.rabah.93@gmail.com>
2022-10-24 11:21:50 +0300

    first assignment in Java

---
+++ java/java_fundamentals/your_own_java_program/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Ali");
+        System.out.println("I'm 29 years old");
+        System.out.println("My home town is Ramallah");
+    }
+}
