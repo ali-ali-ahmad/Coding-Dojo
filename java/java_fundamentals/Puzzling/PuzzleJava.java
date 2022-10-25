e5dcae06970f4ea81065e5499d2a2c47d12322b4
alirabah93 <abu.rabah.93@gmail.com>
2022-10-25 11:56:54 +0300

    Puzzle assignment

---
+++ java/java_fundamentals/Puzzling/PuzzleJava.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.*;
+
+
+public class PuzzleJava {
+
+    // getTenRolls
+    // Write a method that will generate and return an array with 10 random numbers between 1 and 20 inclusive. 
+    public ArrayList<Integer> getTenRolls() {
+
+        ArrayList<Integer> randomOfTen = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        for (int i = 1; i <=10; i++) {
+            randomOfTen.add(rand.nextInt(20) + 1);
+        }
+
+        return randomOfTen;
+    }
+
+
+    // getRandomLetter
+    // Write a method that will:
+    // Create an array within the method that contains all 26 letters of the alphabet 
+    // (this array must have 26 values). 
+    // Generate a random index between 0-25, and use it to pull a random letter out of the array.
+    // Return the random letter.
+    public String getRandomLetter() {
+        Random rand = new Random();
+        
+        String alphabetString = "abcdefghijklmnopqrstuvwxyz";
+        String[] alphabet = new String[26];
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = String.valueOf(alphabetString.charAt(i));
+        }
+        return alphabet[rand.nextInt(26)];
+    }
+
+
+    // generatePassword
+    // Write a method that uses the previous method to create a random string of eight 
+    // characters, and return that string.
+    public String generatePassword() {
+
+        String password = "";
+
+        for(int i = 0; i < 8; i++) {
+            password = password + getRandomLetter();
+        }
+        return password;
+    }
+
+
+    // getNewPasswordSet
+    // Write a method that takes an int length as an argument and creates an array of 
+    // random eight character words. The array should be the length passed in as an int.
+    public ArrayList<String> getNewPasswordSet(int length) {
+
+        ArrayList<String> passwordSet = new ArrayList<String>();
+        for(int i = 0; i < length; i++) {
+            passwordSet.add(generatePassword());
+        }
+        return passwordSet;
+
+    }
+
+
+    // SENSEI BONUS:
+    // shuffleArray
+    // Write a method that takes an array and mixes up all the values in a 
+    // pseudo-random way. Hint: use random indexes within the array, and swap values repeatedly.
+    public String ShuffleArray() {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        Random rand = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            int randomIndexToSwap = rand.nextInt(array.length);
+            int temp = array[randomIndexToSwap];
+            array[randomIndexToSwap] = array[i];
+            array[i] = temp;
+        }
+        return Arrays.toString(array);
+    }
+}
