0b348ebc4664206e9bb714cf58b6fc987a8bcd66
alirabah93 <abu.rabah.93@gmail.com>
2022-12-06 12:41:52 +0200

    ninja assignment

--- MERN/javaScript/OOP/ninja/ninja.js
+++ MERN/javaScript/OOP/ninja/ninja.js
@@ -1,4 +1,3 @@
-// parent Vehicle class
 class Ninja {
     constructor(name, health) {
         this.name = name;
@@ -6,25 +5,29 @@ class Ninja {
         this.speed = 3;
         this.strength = 3;
     }
+
+    sayName() {
+        console.log(`My name is ${this.name}`);
+        return this;
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Health: ${this.health}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Stength: ${this.strength}`);
+        return this;
+    }
+
     drinkSake() {
+        console.log(`${this.name} drank sake and gained 10 health units.`);
         this.health += 10;
-        console.log(`You drank your sake and it now has ${this.health} health on it.`);
+        return this;
     }
-    sayName(name) {
-        this.name = name;
-    }
-    showStats(name, health) {
-        this.name = name;
-        this.health = health;
-    }
-    
 }
 
-    const ninja1 = new Ninja("Hyabusa", 100);
-    ninja1.sayName();
-    ninja1.showStats();
-    console.log(ninja1);
-
-
-
-
+const ninja1 = new Ninja("Hyabusa", 100);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
