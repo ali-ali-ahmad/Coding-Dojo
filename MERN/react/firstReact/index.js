2df574c4ce4e549b3f5e624f16b44a9bf7996559
alirabah93 <abu.rabah.93@gmail.com>
2022-12-06 20:42:31 +0200

    First React assignment

---
+++ MERN/react/firstReact/index.js
@@ -0,0 +1,29 @@
+// // ExampleButton component
+// class ExampleButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { isCLicked: false };
+//     }
+//     render() {
+//         if (this.state.isCLicked) {
+//             return 'You clicked this';
+//         }
+//         return React.createElement(
+//             'button',
+//             { onClick: () => this.setState({ isCLicked: true }) },
+//             'Click me!'
+//         );
+//     }
+// }
+
+// // select root div
+// const domContainer = document.querySelector('#root');
+// // create the root of react dom
+// const root = ReactDOM.createRoot(domContainer);
+// // render what we see
+// root.render(React.createElement(ExampleButton));    
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement("h1", {}, "Our First React page has rendered"));    
+
