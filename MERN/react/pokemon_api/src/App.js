80bdd8ec0f39f950b91fb8621d20cf59e1f495b7
alirabah93 <abu.rabah.93@gmail.com>
2022-12-13 13:52:44 +0200

    pokemon api assignment

--- MERN/react/pokemon_api/src/App.js
+++ MERN/react/pokemon_api/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Effect from './Components/Effect';
-// import Test from './Components/Test';
-// import Flipping from './Components/Flipping';
+
 
 function App() {
   return (
-    // <Flipping/>
+
     <Effect/>
 
   );
